5e32df047db8c1ded0c8e770420f73c49b678a65
Tom Lausmann <tomlausmann@gmail.com>
2023-12-17 15:43:41 +0100

    Move Turn related configurations to TurnConfiguration.kt

--- src/main/kotlin/de/lausi95/gameofthrees/adapter/internal/GameConfiguration.kt
+++ src/main/kotlin/de/lausi95/gameofthrees/adapter/internal/GameConfiguration.kt
@@ -2,34 +2,12 @@ package de.lausi95.gameofthrees.adapter.internal
 
 import de.lausi95.gameofthrees.domain.model.game.RandomStartNumberGenerator
 import de.lausi95.gameofthrees.domain.model.game.StartNumberGenerator
-import de.lausi95.gameofthrees.domain.model.turn.AUTOMATIC_MOVE_RESOLVER
-import de.lausi95.gameofthrees.domain.model.turn.MANUAL_MOVE_RESOLVER
-import de.lausi95.gameofthrees.domain.model.turn.MoveResolver
-import org.slf4j.LoggerFactory
-import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
 @Configuration
 private class GameConfiguration {
 
-  private val log = LoggerFactory.getLogger(this.javaClass)
-
   @Bean
   fun startValueStrategy(): StartNumberGenerator = RandomStartNumberGenerator()
-
-  @Bean
-  @ConditionalOnExpression("'\${game-of-three.move-resolver}'.equals('MANUAL')")
-  fun manualMoveResolver(): MoveResolver {
-    log.info("Using manual move resolver.")
-    return MANUAL_MOVE_RESOLVER
-  }
-
-  @Bean
-  @ConditionalOnMissingBean
-  fun autoMoveResolver(): MoveResolver {
-    log.info("Using automatic move resolver.")
-    return AUTOMATIC_MOVE_RESOLVER
-  }
 }
